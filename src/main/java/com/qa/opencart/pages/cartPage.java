3717ccfe0d120fabc51d9d485681d095a3779087
milanpatel9509 <milanpatel9509@gmail.com>
2022-11-02 10:28:57 -0400

    updated cart page as per arpan comment

--- src/main/java/com/qa/opencart/pages/cartPage.java
+++ src/main/java/com/qa/opencart/pages/cartPage.java
@@ -1,7 +1,16 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class cartPage {
 
+	
+	private By cart=By.id("cart");
+	
+	public void addTocart() {
+		System.out.println("add to cart done");
+	}
+	
 	public static void main(String[] args){
 		System.out.println("cart page");
 	}
